932d7aa66e2718cc91c1849632997239403fe883
Kenny <kennyhuynh2019@gmail.com>
2017-07-26 21:23:37 -0700

    fourth commit

--- chancemeuci/urls.py
+++ chancemeuci/urls.py
@@ -5,13 +5,13 @@ app_name = 'chancemeuci'
 
 urlpatterns = [
     # /chancemeuci/
-    url(r'^$', views.index.as_view(), name = 'index'),
+    url(r'^$', views.index.as_view(), name='index'),
     # /chancemeuci/datasource
-    url(r'^datasource/$', views.datasource, name = 'datasource'),
+    url(r'^datasource/$', views.datasource, name='datasource'),
     # /chancemeuci/development
     url(r'^development/$', views.development, name='development'),
     # /chancemeuci/comments
     url(r'^comments/$', views.comments, name='comments'),
     # /chancemeuci/notes
-    url(r'^notes/$', views.notes, name = 'notes'),
+    url(r'^notes/$', views.notes, name='notes'),
 ]
